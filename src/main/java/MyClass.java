50ac4ca2c6d6a994287149e8230992c26d3a333f
GUPTADKG <devendrakumar.gupta@gmail.com>
2018-09-14 23:00:27 +0530

    forth commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,7 @@
+
+public class MyClass {
+	public int multiply (int i, int j) {
+		return i*j;
+	}
+
+}
---
+++ src/test/java/MyTestClass.java
@@ -0,0 +1,58 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author edragta
+ *
+ */
+public class MyTestClass {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		MyClass tester=new MyClass();
+		 // assert statements
+        assertTrue(tester.multiply(10, 10)==100);
+        assertTrue(tester.multiply(10, 1)==10);
+        assertTrue(tester.multiply(10, 11)==110);
+		//fail("Not yet implemented");
+	}
+	
+
+}
