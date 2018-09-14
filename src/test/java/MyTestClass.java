ec70bb127881847b5732c32225d5b64c3a7ea08e
GUPTADKG <devendrakumar.gupta@gmail.com>
2018-09-15 00:05:30 +0530

    Edited myTest

--- src/test/java/MyTestClass.java
+++ src/test/java/MyTestClass.java
@@ -2,12 +2,12 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
+//import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import sun.security.util.Length;
+//import sun.security.util.Length;
 
 /**
  * 
